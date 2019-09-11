7f9b869eaee8a9ae71a144ed8bd1172a8a7eab6a
carolinecamille <54897741+carolinecamille@users.noreply.github.com>
2019-09-11 16:43:42 +0200

    deploy

--- deploy.R
+++ deploy.R
@@ -5,4 +5,6 @@ setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                token  = Sys.getenv("shinyapps_token"),
                secret = Sys.getenv("shinyapps_secret"))
 
-deployApp()
+deployApp('C:/Users/Caroline/Documents/M2/SVM/projet/projet_final')
+
+
